6565af3f1172d4c2b6f66da47d0d3ff7e979409f
pgopi527 <90849530+pgopi527@users.noreply.github.com>
2026-04-10 18:38:21 +0530

    added docker jnj

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull push pgopi527/second
+docker pull push pgopi527/third
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 pgopi527/second
+docker run -d -p 5000:5000 pgopi527/third
